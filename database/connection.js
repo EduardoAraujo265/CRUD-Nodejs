b337c37d7b98d8feff56f577552cc5b05c1b8c16
Eduardo Araujo <EduardoADev@protonmail.com>
2025-08-08 07:50:53 -0300

    Isolando variáveis de ambiente usando .env, limpando comentários, adicionando .gitignore, etc

--- database/connection.js
+++ database/connection.js
@@ -1,8 +1,18 @@
 const mysql = require('mysql2');
+const dotenv = require('dotenv');
 
+// Carregar variáveis de ambiente do arquivo .env
+dotenv.config();
+
+// Configuração da conexão com o banco de dados
+// As variáveis de ambiente devem ser definidas no arquivo .env
+const host = process.env.host;
+const user = process.env.user;
+const password = process.env.password;
+const database = process.env.database;
 module.exports = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "crud_php",
+    host: host,
+    user: user,
+    password: password,
+    database: database
 });
